b8a922d81a5edc12d3e6265d83336e0c6b017825
Thilak Sakkaravarthi <143710776+Thilak2006@users.noreply.github.com>
2026-02-27 10:45:32 +0530

    Delete quiz_project directory

--- quiz_project/quizzes/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.quiz_list, name='quiz_list'),
-    path('<int:quiz_id>/', views.take_quiz, name='take_quiz'),
-]
